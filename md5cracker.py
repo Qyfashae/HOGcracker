bc1a02dfacc678007a5eac8c859da2dd399f8caa
Felix.T <75949095+Qyfashae@users.noreply.github.com>
2022-07-21 14:41:16 +0200

    Add files via upload

---
+++ md5cracker.py
@@ -0,0 +1,12 @@
+import hashlib as hl 
+
+wordlist = str(input('Enter wordlist location: '))
+hash_input = str(input('Enter hash to be cracked: '))
+
+with open(wordlist, 'r') as file:
+	for line in file.readlines()
+		hash_ob = hashlib.md5(line.strip().encode())
+		hashed_pass = hash_ob.hexdigest()
+		if hashed_pass == hash_input:
+			print("Cracked successfully! " + line.strip())
+			exit(0)
