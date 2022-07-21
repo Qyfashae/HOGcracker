4668420365fdf7d96950380e1369220b0f826df2
Felix.T <75949095+Qyfashae@users.noreply.github.com>
2022-07-21 13:43:09 +0100

    Update md5cracker.py

--- md5cracker.py
+++ md5cracker.py
@@ -4,7 +4,7 @@ wordlist = str(input('Enter wordlist location: '))
 hash_input = str(input('Enter hash to be cracked: '))
 
 with open(wordlist, 'r') as file:
-	for line in file.readlines()
+	for line in file.readlines():
 		hash_ob = hashlib.md5(line.strip().encode())
 		hashed_pass = hash_ob.hexdigest()
 		if hashed_pass == hash_input:
